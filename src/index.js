84aced6ee22ee0b32924a60d1e3295c0db300a12
Ulman-huy <buihuy.lh@gmail.com>
2023-04-01 22:16:04 +0700

    xay dung trang create, edit, product

--- src/index.js
+++ src/index.js
@@ -4,26 +4,34 @@ const morgan = require('morgan');
 const db = require('./config/db')
 const route = require('./routes');
 const handlebars = require('express-handlebars');
+const methodOverride = require('method-override');
 const path = require('path');
 const port = 3000;
-
-const app = express();
-app.use(express.urlencoded());  
-app.use(cors()); 
-app.use(morgan('combined')); 
-   
+ 
+const app = express();    
+app.use(express.urlencoded());
 db.connect(); // Connect database
+
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'))
+
+app.use(cors());
+app.use(morgan('combined'));
 
 app.use(express.static(path.join(__dirname, 'public'))); 
 
 app.engine('hbs', handlebars.engine({ 
-    extname: '.hbs' 
+    extname: '.hbs',
+    helpers: {
+        formatDate: date => {
+            var newsDate  = new Date(date)
+            return newsDate.toLocaleDateString("en-US")
+        }
+    }
 }))  
 app.set('view engine', '.hbs');
 app.set('views', './src/views');
 
-route(app);
+route(app); 
 
 app.listen(port, () => console.log('Server is running on port: ', port));
-
-// test
--- src/models/index.js
+++ src/models/index.js
@@ -5,13 +5,16 @@ mongoose.plugin(slug);
 
 const coffeeSchema = new mongoose.Schema({
     name: { type: String, required: true},
+    brand: {type: String, default: 'no brand'},
     price: {type: Number, required: true},
     star: {type: Number, default: 5},
     like: {type: Boolean, default: false},
-    sale: {type: Number},
+    sale: {type: Number, default: 0},
     brand: {type: String},
-    description: {type: String, required: true},
-    image: {type: String},
+    description: {type: String},
+    info: {type: String},
+    images: {type: Array},
+    date: {type: Date},
     slug: {type: String, slug: 'name'}
 }, {
     timestamps: true
