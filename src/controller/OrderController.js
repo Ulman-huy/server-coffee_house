a9fb7ccde746c81beb75c0895f337aaf78fbb5a9
Ulman-huy <buihuy.lh@gmail.com>
2023-11-25 21:00:41 +0700

    re-build

--- src/controller/OrderController.js
+++ src/controller/OrderController.js
@@ -1,95 +1,19 @@
 const ObjectId = require("mongoose").Types.ObjectId;
 const { Package } = require("../models/package");
 const { Product } = require("../models/product");
-const { Image } = require("../models/image");
 const { multipleMongooseToObject } = require("../util/mongoose");
 const serverUrl = process.env.SERVER;
 
 class OrderController {
   getAllPackage(req, res, next) {
-    Package.find({}).then((packages) =>
-      res.render("package", { packages: multipleMongooseToObject(packages) })
-    );
+   
   }
   // [POST] /user/new-package
   newPackage(req, res, next) {
-    const newPackage = new Package({
-      ...req.body,
-      _id: new ObjectId(),
-      userId: req.body._id,
-    });
-    newPackage
-      .save()
-      .then(() => res.status(200).json({ message: "Đặt đơn hàng thành công!" }))
-      .catch((err) =>
-        res
-          .status(500)
-          .json({ message: "Có lỗi xảy ra. Vui lòng thử lại sau!" })
-      );
+    
   }
   getPackageDetail(req, res, next) {
-    let id_products = [];
-    let pkgInfo = {};
-    let qnt;
-
-    Package.findById({ _id: req.params.id })
-      .then((pkg) => {
-        pkgInfo = pkg;
-        pkg.cart.map((product) => id_products.push(product.id_product));
-        return id_products;
-      })
-      .then((id_products) => {
-        Product.find({ _id: { $in: id_products } })
-          .populate("images")
-          .exec()
-          .then((products) => {
-            const result = products.map((product) => {
-              const imageIds = product.images;
-              const images = [];
-              return Promise.all(
-                imageIds.map((imageId) => {
-                  return Image.findById(imageId).then((image) => {
-                    if (image) {
-                      images.push(image);
-                    }
-                  });
-                })
-              ).then(() => {
-                const newCart = Object.values(pkgInfo.cart);
-                newCart.forEach((item) => {
-                  if (item.id_product === product.id) {
-                    qnt = item.quantity;
-                  }
-                });
-                return {
-                  _id: product._id,
-                  type: product.type,
-                  name: product.name,
-                  brand: product.brand,
-                  quantity: qnt,
-                  price: product.price,
-                  sale: product.sale,
-                  src: images.map((image) => serverUrl + image.path.slice(11)),
-                };
-              });
-            });
-            return Promise.all(result);
-          })
-          .then((result) => {
-            const pkg = {
-              id: pkgInfo._id,
-              name: pkgInfo.name,
-              phone: pkgInfo.phone,
-              status: pkgInfo.status,
-              time: pkgInfo.time,
-              location: pkgInfo.location,
-              message: pkgInfo.message,
-              packages: [...result],
-              cart: pkgInfo.cart,
-            };
-            res.render("package-detail", { package: pkg });
-          });
-      });
+   
   }
 }
 
--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -1,6 +1,5 @@
 const { Package } = require("../models/package");
 const { Product } = require("../models/product");
-const { Image } = require("../models/image");
 const { User } = require("../models/user");
 const { randomToken } = require("../util");
 const { sendEmailForgotPassword } = require("../util/mailer");
@@ -12,126 +11,15 @@ const serverUrl = process.env.SERVER;
 class UserController {
   // [POST] /user/getCart
   getCart(req, res, next) {
-    const { user } = req.body;
-    User.findOneAndUpdate({ _id: user._id }, user)
-      .then(() => console.log("Cập nhật giỏ hàng thành công!"))
-      .catch((err) => console.log(err));
-
-    let id_products = [];
-    user.cart.map((cart) => id_products.push(cart.id_product));
-    Product.find({ _id: { $in: id_products } })
-      .populate("images")
-      .exec()
-      .then((products) => {
-        const result = products.map((product) => {
-          const imageIds = product.images;
-          const images = [];
-          return Promise.all(
-            imageIds.map(async (imageId) => {
-              return Image.findById(imageId).then((image) => {
-                if (image) {
-                  images.push(image);
-                }
-              });
-            })
-          ).then(() => {
-            return {
-              _id: product._id,
-              type: product.type,
-              name: product.name,
-              brand: product.brand,
-              price: product.price,
-              star: product.star,
-              sale: product.sale,
-              description: product.description,
-              info: product.info,
-              src: images.map((image) => serverUrl + image.path.slice(11)),
-              slug: product.slug,
-            };
-          });
-        });
-        return Promise.all(result);
-      })
-      .then((result) => {
-        res.status(200).json(result);
-      });
+    
   }
   // [PUT] /user/upadte-location
   updataLocation(req, res, next) {
-    User.findOneAndUpdate({ _id: req.body.userId }, req.body).then((user) =>
-      console.log("Cập nhật thông tin thành công!")
-    );
-
-    User.findById(req.body.userId).then((user) => res.status(200).json(user));
+   
   }
   // [GET] /user/package
   getPackage(req, res, next) {
-    Package.find({ userId: req.query.id })
-      .then((pkgs) => {
-        const pkgInfoList = pkgs.map((pkg) => pkg.toObject());
-        const id_products = pkgInfoList.reduce((ids, pkg) => {
-          pkg.cart.forEach((item) => ids.push(item.id_product));
-          return ids;
-        }, []);
-        return Product.find({ _id: { $in: id_products } })
-          .populate("images")
-          .exec()
-          .then((products) => {
-            const result = products.map(async (product) => {
-              const imageIds = product.images;
-              const images = [];
-              for (let i = 0; i < imageIds.length; i++) {
-                const image = await Image.findById(imageIds[i]);
-                if (image) {
-                  images.push(image);
-                }
-              }
-              const pkg = pkgInfoList.find((pkg) => {
-                return pkg.cart.some(
-                  (item) =>
-                    item.id_product.toString() === product._id.toString()
-                );
-              });
-              const qnt = pkg.cart.find(
-                (item) => item.id_product === product.id
-              ).quantity;
-              return {
-                _id: product._id,
-                type: product.type,
-                name: product.name,
-                brand: product.brand,
-                quantity: qnt,
-                price: product.price,
-                sale: product.sale,
-                src: images.map((image) => serverUrl + image.path.slice(11)),
-              };
-            });
-            return Promise.all(result).then((pkgProducts) => {
-              return (pkgs = pkgInfoList.map((pkg) => {
-                const pkgResult = pkgProducts.filter((product) => {
-                  return pkg.cart.some(
-                    (item) =>
-                      item.id_product.toString() === product._id.toString()
-                  );
-                });
-                console.log(pkgResult);
-                return {
-                  id: pkg._id,
-                  name: pkg.name,
-                  phone: pkg.phone,
-                  status: pkg.status,
-                  time: pkg.time,
-                  location: pkg.location,
-                  message: pkg.message,
-                  packages: [...pkgResult],
-                  cart: pkg.cart,
-                };
-              }));
-            });
-          });
-      })
-      .then((result) => res.status(200).json(result))
-      .catch(next);
+   
   }
   async getMe(req, res) {
     return res.status(201).json(req.user);
