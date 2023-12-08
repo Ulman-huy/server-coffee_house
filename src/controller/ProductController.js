dbe35e1d3918ec1dd6424ca5c3496878aba28497
Ulman-huy <buihuy.lh@gmail.com>
2023-12-08 10:12:33 +0700

    save

--- src/controller/ProductController.js
+++ src/controller/ProductController.js
@@ -145,7 +145,7 @@ class ProductController {
         const productExist = user.cart.find((item) => item.product_id.toString() === _id);
         if (productExist) {
           const newCart = user.cart.map((item) => {
-            if (item.product_id === _id) {
+            if (item.product_id.toString() === _id) {
               return {
                 ...item,
                 quantity: (item.quantity += quantity),
@@ -158,7 +158,7 @@ class ProductController {
           user.cart.push({ product_id: _id, quantity: quantity });
         }
         user.save();
-        return res.status(200).json({ message: "OK", user });
+        return res.status(200).json({ message: "OK" });
       }
       if (type === "MINUS") {
         const productExist = user.cart.find((item) => item.product_id.toString() === _id);
@@ -177,7 +177,7 @@ class ProductController {
             .filter((item) => item.quantity > 0);
           user.cart = newCart;
           user.save();
-          return res.status(200).json({ message: "OK", user });
+          return res.status(200).json({ message: "OK" });
         } else {
           return res
             .status(400)
