7edc1673c86fe226cfc7d74f4484cadb851e4ce1
Ulman-huy <buihuy.lh@gmail.com>
2023-12-12 11:51:24 +0700

    save

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connect = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://tensenit:8mzOR8gMwCgquRwW@coffee.ztf1ifr.mongodb.net/?retryWrites=true&w=majority",
+      "mongodb+srv://devyyuh:0DLgARLGRt5DXkN0@coffee.u667nzn.mongodb.net/?retryWrites=true&w=majority",
       {
         useNewUrlParser: true,
       }
